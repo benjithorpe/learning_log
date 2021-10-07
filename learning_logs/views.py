d4b336f8592991cdf784f185c607755bf1cdc8a0
benjithorpe <benjaminthorpe19@gmail.com>
2021-10-07 05:43:04 +0000

    Add a topic page, now able to access a single topic and its entries

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -19,6 +19,7 @@ def topics(request):
 def topic(request, topic_id):
     """Show a single topic and all its entries."""
     topic = Topic.objects.get(id=topic_id)
-    entries = topic.entry_set.order_by("date_added")
+    # The minus sign in front of date_added sorts the result in reverse order.
+    entries = topic.entry_set.order_by("-date_added")
     context = {"topic": topic, "entries": entries}
     return render(request, "learning_logs/topic.html", context)
