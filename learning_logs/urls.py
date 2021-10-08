ef73abf684348044166044966b103a7589ad5bc8
benjithorpe <benjaminthorpe19@gmail.com>
2021-10-08 08:36:03 +0000

    Add an edit functionality to the entries

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,8 +7,14 @@ from . import views
 app_name = "learning_logs"
 urlpatterns = [
     path("", views.index, name="index"),
-    # page that shows all available topics
+    # Page that shows all available topics
     path("topics/", views.topics, name="topics"),
-    # detailed page for a single topic
-    path("topics/<int:topic_id>/", views.topic, name="topic")
+    # Detailed page for a single topic
+    path("topics/<int:topic_id>/", views.topic, name="topic"),
+    # Page for adding new topic
+    path("new_topic/", views.new_topic, name="new_topic"),
+    # Page for adding new entry
+    path("new_entry/<int:topic_id>/", views.new_entry, name="new_entry"),
+    # Page for editing an entry
+    path("edit_entry/<int:entry_id>/", views.edit_entry, name="edit_entry"),
 ]
